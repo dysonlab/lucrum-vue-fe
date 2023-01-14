d717579328614cc356849286a060849a8dee5de6
Reizkian <reizkian@vouchconcierge.com>
2023-01-14 23:37:26 +0700

    Add UnderDevelopment.vue

--- src/store/action.js
+++ src/store/action.js
@@ -14,12 +14,27 @@ const actions = {
                 dispatch("ERROR_HANDLE", error)
             })
     },
+    async REGISTER({dispatch}, payload){
+        api.reigster(payload)
+            .then((response)=>{
+                console.log(response)
+            })
+            .catch((error)=>{
+                console.log(error)
+                dispatch("ERROR_HANDLE", error)
+            })
+    },
     ERROR_HANDLE({ commit }, error){
         console.log("HANDLE_ERROR()", error.response.status)
         switch(error.response.status){
             case 401:
                 commit("SET_ERROR_MODAL", true);
                 commit("SET_ERROR_MESSAGE", "You have entered an invalid email or password")
+                break;    
+            case 409:
+                commit("SET_ERROR_MODAL", true);
+                commit("SET_ERROR_MESSAGE", "User is already exists")
+                break;
         }
     }
 }
---
+++ src/views/UnderDevelopment.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="container">
+        <img src="@/assets/under_development.png" alt="">
+        <h1>Under Development</h1>
+        <div class="content">
+            <p>Please contact <em>dysonlab.software@gmail.com</em> if you want to contribute as a developer. The
+                repository of the
+                project can be found <a target="_blank" href="https://github.com/dysonlab?tab=repositories">here</a>,
+                and the specification document <a target="_blank" href="https://docs.google.com/document/d/17fEEuoh7yv-vcTBmORrTu9X0IpCL-nL7YZizq6MKfMg/edit?usp=sharing">here</a>.</p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+    padding: 10px;
+
+    text-align: center;
+
+    img {
+        width: 50%;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    h1 {
+        font-weight: 400;
+    }
+
+    .content {
+        max-width: 50%;
+        margin-left: auto;
+        margin-right: auto;
+
+        p {
+            padding: 30px;
+            font-weight: 200;
+            text-align: center;
+        }
+
+        a {
+            color: blue;
+        }
+    }
+
+    @media (max-width: 800px) {
+        img {
+            width: 100%;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        .content {
+            max-width: 100%;
+            margin-left: auto;
+            margin-right: auto;
+        }
+    }
+}
+</style>
